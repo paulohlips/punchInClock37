0841d0409870d30603c551a64d55ad12f2462083
paulohlips <paulohfcmendes@gmail.com>
2022-08-22 18:51:43 -0300

    feat: add config to make a build

--- main.js
+++ main.js
@@ -1,26 +1,19 @@
 const { app, BrowserWindow } = require('electron')
 const path = require('path')
 
-require("electron-reload")(__dirname, {
-  electron: require(`${__dirname}/node_modules/electron`),
-})
-
 function createWindow() {
   const win = new BrowserWindow({
+    autoHideMenuBar: true,
+    icon: './src/render/assets/clock.ico',
     width: 800,
     height: 600,
     webPreferences: {
       nodeIntegration: true,
       contextIsolation: false,
-    },
-    icon: path.join(__dirname + '/src/render/assets/excel-spreadsheet-icon-file-type-xls-icon-740169.icns')
+    }
   })
 
-
-
   win.loadFile('src/render/html/index.html')
-
-  win.webContents.openDevTools()
 }
 app.whenReady().then(createWindow)
 app.on('window-all-closed', () => {
